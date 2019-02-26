006cb9848e71e2f371ae6e9764831a48dbf400bb
LukeDTurner <luketurnertx@gmail.com>
2019-02-26 13:44:29 -0600

    Update employeetable.sql

--- employeetable.sql
+++ employeetable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employee ( 
-  record_id uuid NOT NULL DEFAULT gen_random_uuid(), --The record ID. Stored in the edu.uark.dataaccess.entities:BaseEntity#id property. See also the named constant defined in edu.uark.dataaccess.entities:BaseFieldNames that is used for Java <-> SQL mappings.
+  record_id SERIAL NOT NULL,
   first_name varchar(16),
   last_name varchar(16),
   employee_id integer NOT NULL,
@@ -7,11 +7,13 @@ CREATE TABLE employee (
   role varchar(16),
   manager integer,
   password varchar(16) NOT NULL,
-  created_on timestamp without time zone NOT NULL DEFAULT now(), --Stored in the edu.uark.dataaccess.entities:BaseEntity#created_on property. See also the named constant defined in edu.uark.dataaccess.entities:BaseFieldNames that is used for Java <-> SQL mappings.
+  created_on timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT pk_employee PRIMARY KEY (record_id),
   CONSTRAINT fk_manager FOREIGN KEY (manager) REFERENCES employee(record_id),
   CONSTRAINT chk_employee_id CHECK (employee_id between 0 and 99999),
-  CONSTRAINT chk_role CHECK (role IN ('General Manager', 'Shift Manager', 'Cashier')
-) WITH (
+  CONSTRAINT chk_role CHECK (role IN ('General Manager', 'Shift Manager', 'Cashier'))
+);
+                             
+                             WITH (
   OIDS=FALSE
 );
