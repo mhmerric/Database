86f9aa3385696d386cfbfd457e4cf48558ad30ca
LukeDTurner <luketurnertx@gmail.com>
2019-04-09 11:33:39 -0500

    Update transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -5,19 +5,29 @@ CREATE TABLE transaction (
   sale_id SERIAL NOT NULL,
   total float,
   payment varchar(16),
-  employee_id
-  customer_id
+  employee_id int NOT NULL DEFAULT(0),
+  customer_id int NOT NULL DEFAULT(0),
+  store_id int NOT NULL DEFAULT(0),
+  created_on timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT pk_transaction PRIMARY KEY (sale_id),
   CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+  CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+  CONSTRAINT fk_store FOREIGN KEY (store_id) REFERENCES store(store_id)
 );
 
 DROP TABLE transaction_entry;
 
 CREATE TABLE transaction_entry (
-  product_id SERIAL NOT NULL,
-  sale_id SERIAL NOT NULL,
+  product_id NOT NULL DEFAULT(0),
+  sale_id NOT NULL DEFAULT(0),
   quanity integer NOT NULL,
   subtotal float,
   CONSTRAINT pk1_transaction_entry PRIMARY KEY (product_id),
-  CONSTRAINT pk2_transaction_entry PRIMARY KEY (sale_id),
+  CONSTRAINT pk2_transaction_entry PRIMARY KEY (sale_id)
+);
+
+CREATE TABLE customer (
+  customer_id SERIAL NOT NULL,
+  phone varchar(16),
+  CONSTRAINT pk_customer PRIMARY KEY (customer_id)
 );
