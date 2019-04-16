7f3ceb9fe03f91e1ce1b8e2db461766879dede4d
Spencer Heald <spencer.heald@gmail.com>
2019-04-16 10:22:10 -0500

    Update inventory.sql

--- inventory.sql
+++ inventory.sql
@@ -1,11 +1,11 @@
 
 
 CREATE TABLE inventory (
-    store_ID INT NOT NULL,
-    product_ID INT NOT NULL,
-    quantity INT,
-    CONSTRAINT PK_inventory PRIMARY KEY (store_ID, product_ID),
-    CONSTRAINT CHK_quantity CHECK (quantity >= 0)
+    store_ID INT NOT NULL, -- ID of store Unique for each store, but not unique for each row.
+    product_ID INT NOT NULL, -- ID of product Unique for each product, but not unique for each store.
+    quantity INT, -- Quantity of Product left in related store.
+    CONSTRAINT PK_inventory PRIMARY KEY (store_ID, product_ID), -- Primary key is combination of StoreID and ProductID. Together they are unique for each entry
+    CONSTRAINT CHK_quantity CHECK (quantity >= 0) -- Quantity should never be allowed to go below 0.
 );
 
 
