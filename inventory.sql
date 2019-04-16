6a33a5db676e1d424a12f5fb6f5a128f75e6b6e0
Spencer Heald <spencer.heald@gmail.com>
2019-04-16 10:09:15 -0500

    Update inventory.sql

--- inventory.sql
+++ inventory.sql
@@ -1,7 +1,16 @@
 
 
-Create TABLE inventory (
+CREATE TABLE inventory (
     store_ID INT,
     product_ID INT,
-    quantity INT
-)
+    quantity INT,
+    CONSTRAINT PK_inventory PRIMARY KEY (store_ID, product_ID),
+    CONSTRAINT CHK_quantity CHECK (quantity >= 0)
+);
+
+
+INSERT INTO inventory (store_ID, product_ID, quantity) VALUES (
+    01,
+    02,
+    35
+    )
