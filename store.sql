1780137d1572dac1231c977d502ceed17900aa89
Spencer Heald <spencer.heald@gmail.com>
2019-04-16 10:41:25 -0500

    Updated store.sql to include basic setup.
    added basic setup and a test store location.

--- store.sql
+++ store.sql
@@ -1,1 +1,10 @@
+CREATE TABLE store (
+    store_ID INT NOT NULL, -- PK and ID of store unique for each row.
+    location varchar(20),
+    constraint PK_store_ID PRIMARY KEY (store_ID)
+  );
 
+INSERT INTO store (store_ID, location) VALUES (
+  01,
+  Fayetteville
+  )
