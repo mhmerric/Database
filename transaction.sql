0c28e4f2e66e1dc78ab8558a9e5473ccf03128aa
Spencer Heald <spencer.heald@gmail.com>
2019-04-29 11:53:24 -0500

    Update transaction.sql
    Removed customer and store identifications, reducing work for server developers.

--- transaction.sql
+++ transaction.sql
@@ -6,13 +6,9 @@ CREATE TABLE transaction (
   total float,
   payment varchar(16),
   employee_id int NOT NULL DEFAULT(0),
-  customer_id int NOT NULL DEFAULT(0),
-  store_id int NOT NULL DEFAULT(0),
   created_on timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT pk_transaction PRIMARY KEY (sale_id),
   CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
-  CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-  CONSTRAINT fk_store FOREIGN KEY (store_id) REFERENCES store(store_id)
 );
 
 DROP TABLE transaction_entry;
@@ -23,10 +19,4 @@ CREATE TABLE transaction_entry (
   quanity integer NOT NULL,
   subtotal float,
   CONSTRAINT pk1_transaction_entry PRIMARY KEY (product_id, sale_id)
-);
-
-CREATE TABLE customer (
-  customer_id SERIAL NOT NULL,
-  phone varchar(16),
-  CONSTRAINT pk_customer PRIMARY KEY (customer_id)
 );
